8729ab0bb437140d8b3a1988bab225c9e92ab353
OSUTristanH <harvillt@oregonstate.edu>
2021-07-25 23:12:35 -0500

    Added an admin page
    Added a page for each table that allows for table manipulation in the database.

--- app.py
+++ app.py
@@ -61,8 +61,54 @@ def register():
 def experts():
     return render_template('experts.html')
 
+@app.route('/admins')
+def admins():
+    return render_template('admins.html')
+
+@app.route('/admins/users')
+def adminsusers():
+    cursor.execute("SELECT * FROM `Users`")
+    data = cursor.fetchall()
+    return render_template('adminusers.html', data=data)
+
+@app.route('/admins/plants')
+def adminsplants():
+    cursor.execute("SELECT * FROM `Plants`")
+    data = cursor.fetchall()
+    return render_template('adminplants.html', data=data)
+
+@app.route('/admins/care')
+def adminscare():
+    cursor.execute("SELECT * FROM `Care`")
+    data = cursor.fetchall()
+    return render_template('admincare.html', data=data)
+
+@app.route('/admins/guides')
+def adminsguides():
+    cursor.execute("SELECT * FROM `Guides`")
+    data = cursor.fetchall()
+    return render_template('adminguides.html', data=data)
+
+@app.route('/admins/experts')
+def adminsexperts():
+    cursor.execute("SELECT * FROM `Experts`")
+    data = cursor.fetchall()
+    return render_template('adminexperts.html', data=data)
+
+@app.route('/admins/plantsowned')
+def adminspo():
+    cursor.execute("SELECT * FROM `PlantsOwned`")
+    data = cursor.fetchall()
+    return render_template('adminpo.html', data=data)
+
+@app.route('/admins/userexperts')
+def adminsue():
+    cursor.execute("SELECT * FROM `UserExpert`")
+    data = cursor.fetchall()
+    return render_template('adminue.html', data=data)
+
 # Listener
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 7777))
-    app.run(port=port)
+    app.run(port=port, debug=True)
